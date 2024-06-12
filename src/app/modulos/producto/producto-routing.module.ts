fcb9dd0636d25f353d250ac7b1661ef30bd6a2d6
JonathanArroyaveGonzalez <arroyave3000g@gmail.com>
2024-06-12 16:39:52 -0500

    Adición sección misCompras

--- src/app/modulos/producto/producto-routing.module.ts
+++ src/app/modulos/producto/producto-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { PostComponent } from './post/post.component';
 import { PostInfoComponent } from './post-info/post-info.component';
 import { ReviewComponent } from './review/review.component';
+import { My_postComponent } from './my_post/my_post.component';
+import { MisComprasComponent } from './mis-compras/mis-compras.component';
 
 
 
@@ -14,6 +16,11 @@ const routes: Routes = [
 { path: "post-info/:id", component: PostInfoComponent },
 
 { path: "post-review", component: ReviewComponent },
+
+{ path: "my-post", component: My_postComponent },
+
+{ path: "mis-compras", component: MisComprasComponent },
+
 ];
 
 @NgModule({
