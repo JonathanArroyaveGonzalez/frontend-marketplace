d7a3d9a02ce2950c649d8a56d829a52ad539cb9a
JonathanArroyaveGonzalez <arroyave3000g@gmail.com>
2024-06-13 02:44:04 -0500

    configuracion ruta backend

--- src/app/config/configuracion.rutas.backend.ts
+++ src/app/config/configuracion.rutas.backend.ts
@@ -1,3 +1,5 @@
 export namespace ConfiguracionRutasBackend{
-    export const urlBackend:string = "http://127.0.0.1:8000/";
+/*     export const urlBackend:string = "http://127.0.0.1:8000/";
+ */
+export const urlBackend:string = "https://marketplace-backend-wat6.onrender.com/";
 }
